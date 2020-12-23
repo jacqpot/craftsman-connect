45d1ac9d15ef65486699a88c4a030641fb7a92a2
Jacques_Potgieter <Potgieter.g.jacques@gmail.com>
2020-12-23 13:30:06 -0700

    devise login works, google omniauth still in process

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,9 @@ class ApplicationController < ActionController::Base
     def after_sign_in_path_for(resource)
         request.env['omniauth.origin'] || root_path
     end
-
+    def after_sign_out_path_for(resource)
+        root_path
+    end
     def has_permission(object)
         object.user == current_user 
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :users
- 
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
-  root 'users#show'
+  resources :users
+  resources :guests
+  root 'guests#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
