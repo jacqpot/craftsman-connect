c56d6b8c369b981f9dd32691e7437b1f39be6f42
Jacques_Potgieter <Potgieter.g.jacques@gmail.com>
2020-12-23 13:10:25 -0700

    fix incorrect omniauth proovider info

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :users
  
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
-  root 'user#show'
+  root 'users#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
